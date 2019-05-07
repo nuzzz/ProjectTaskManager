1a7d6120719a23ccb1c2e5c8a7dad88682448a1a
nuzzz <fuzznuzz@gmail.com>
2019-05-07 16:57:40 +0800

    first commit'

---
+++ src/main/java/ProjectTaskManager.java
@@ -0,0 +1,9 @@
+public class ProjectTaskManager {
+    public static void main(String args[]){
+//        4.	Load files from s3
+//        5.	Daily insert new tasks if they start today
+//        6.	Collect new tasks
+        System.out.println("Put this class in a jar file");
+    }
+
+}
